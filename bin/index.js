d75df3610ef5894ae0f6e812821362d143cf4ce1
Daniel Vasylenko <danielspr3@gmail.com>
2020-04-23 02:20:33 +0200

    Unzipping. Bug with cyrilic names

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,9 @@
 
 const chalk = require('chalk');
 const yargs = require('yargs');
+const fs = require('fs');
+const path = require('path');
+const unzipper = require('unzipper');
 
 const options = yargs
   .usage('Usage -src <path>')
@@ -13,4 +16,20 @@ const options = yargs
   })
   .argv;
 
-console.log(`Hello, ${options.src}`);
+const extension = path.extname(options.src);
+const filename = path.basename(options.src);
+
+if (extension !== '.siq') {
+  console.error('Unsupported file extension');
+  process.exit(1);
+}
+
+
+const filepath = path.resolve(process.cwd(), options.src);
+
+console.log('path', filepath);
+
+fs.createReadStream(filepath)
+  .pipe(unzipper.Extract({ path: `./${filename}`, concurrency: 2 }));
+
+console.log('Unzipped');
