af20347c1936b060ca3dccdd14c95a95273b700f
Daniel Vasylenko <danielspr3@gmail.com>
2020-04-25 21:06:25 +0200

    Fix bug with cyrylic file names. WIP - parsing root file

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,13 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
 const fs = require('fs');
+const { promisify } = require('util');
 const path = require('path');
 const unzipper = require('unzipper');
+const xml2js = require('xml2js');
+const il = require('iconv-lite');
+
+const readFilePromise = promisify(fs.readFile);
 
 const options = yargs
   .usage('Usage -src <path>')
@@ -17,19 +22,56 @@ const options = yargs
   .argv;
 
 const extension = path.extname(options.src);
-const filename = path.basename(options.src);
 
 if (extension !== '.siq') {
   console.error('Unsupported file extension');
   process.exit(1);
 }
 
+const zipFilePath = path.resolve(process.cwd(), options.src);
+const filename = path.basename(options.src).replace(extension, '-temp');
+const unzippedFolder = path.resolve(process.cwd(), filename);
+const rootXml = path.normalize(`${unzippedFolder}/content.xml`);
 
-const filepath = path.resolve(process.cwd(), options.src);
+const unzip = (zipFilePath) => {
+  return new Promise((resolve, reject) => {
+    const assertedPaths = [];
+    fs.createReadStream(zipFilePath)
+      .pipe(unzipper.Parse())
+      .on('entry', (entry) => {
+        const decodedFileName = decodeURIComponent(
+          entry.isUnicode
+            ? entry.path
+            : il.decode(entry.props.pathBuffer, 'cp866')
+        );
+        console.log(decodedFileName);
+        const fileDestination = path.join(unzippedFolder, decodedFileName);
+        const directory = path.dirname(fileDestination);
+        if (!assertedPaths.includes(directory) && !fs.existsSync(directory)) {
+          fs.mkdirSync(directory, { recursive: true });
+          assertedPaths.push(directory);
+        }
 
-console.log('path', filepath);
+        entry.pipe(fs.createWriteStream(fileDestination));
+      })
+      .once('close', resolve)
+      .once('error', reject);
+  });
+};
 
-fs.createReadStream(filepath)
-  .pipe(unzipper.Extract({ path: `./${filename}`, concurrency: 2 }));
-
-console.log('Unzipped');
+unzip(zipFilePath)
+  .then(() => {
+    console.log(`Unzipped`);
+    return readFilePromise(rootXml);
+  })
+  .then((xml) => {
+    return xml2js.parseStringPromise(xml);
+  })
+  .then(({ package }) => {
+    const { $, info, rules } = package;
+    console.log('$', $);
+    console.log('info', JSON.stringify(info, null, 2));
+  })
+  .catch((error) => {
+    console.error(chalk.yellow('Failed to parse the file: ', error.message));
+  });
